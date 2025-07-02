38b71889cfc1dc32ffc17be08d60efb691dd9997
Taehyun <ohyeaha@hanmail.net>
2025-07-02 13:39:40 +0900

    [feature] 2s 1b - 1

--- 02_tdd_baseball/baseball.cpp
+++ 02_tdd_baseball/baseball.cpp
@@ -19,6 +19,12 @@ public:
         if (guessNumber == question) {
             return { true, 3, 0 };
         }
+        
+        if (guessNumber[0] == question[0]
+            && guessNumber[1] == question[1]
+            && guessNumber[2] != question[2]) {
+            return { false, 2, 0 };
+        }
 
         return { false, 0, 0 };
     }
--- 02_tdd_baseball/baseball_test.cpp
+++ 02_tdd_baseball/baseball_test.cpp
@@ -29,6 +29,14 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
     EXPECT_EQ(0, result.balls);
 }
 
+TEST_F(BaseballFixture, ReturnResultFor2Strikes0BallFirst) {
+    GuessResult result = game.guess("120");
+
+    EXPECT_FALSE(result.solved);
+    EXPECT_EQ(2, result.strikes);
+    EXPECT_EQ(0, result.balls);
+}
+
 int main() {
     ::testing::InitGoogleMock();
     return RUN_ALL_TESTS();
