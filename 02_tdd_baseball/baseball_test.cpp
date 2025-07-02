ee7583062c3965147443551ec161668337ec8243
Taehyun <ohyeaha@hanmail.net>
2025-07-02 14:09:54 +0900

    [refactor] 1s 2b - 2

--- 02_tdd_baseball/baseball_test.cpp
+++ 02_tdd_baseball/baseball_test.cpp
@@ -21,6 +21,14 @@ public:
         EXPECT_EQ(2, result.strikes);
         EXPECT_EQ(0, result.balls);
     }
+
+    void check1Strikes2Ball(string guessNumber) {
+        GuessResult result = game.guess(guessNumber);
+
+        EXPECT_FALSE(result.solved);
+        EXPECT_EQ(1, result.strikes);
+        EXPECT_EQ(2, result.balls);
+    }
 };
 
 TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
@@ -43,28 +51,10 @@ TEST_F(BaseballFixture, ReturnResultFor2Strikes0Ball) {
     check2Strikes0Ball("023");
 }
 
-TEST_F(BaseballFixture, ReturnResultFor1Strike2BallsFirst) {
-    GuessResult result = game.guess("132");
-
-    EXPECT_FALSE(result.solved);
-    EXPECT_EQ(1, result.strikes);
-    EXPECT_EQ(2, result.balls);
-}
-
-TEST_F(BaseballFixture, ReturnResultFor1Strike2BallsSecond) {
-    GuessResult result = game.guess("321");
-
-    EXPECT_FALSE(result.solved);
-    EXPECT_EQ(1, result.strikes);
-    EXPECT_EQ(2, result.balls);
-}
-
-TEST_F(BaseballFixture, ReturnResultFor1Strike2BallsThird) {
-    GuessResult result = game.guess("213");
-
-    EXPECT_FALSE(result.solved);
-    EXPECT_EQ(1, result.strikes);
-    EXPECT_EQ(2, result.balls);
+TEST_F(BaseballFixture, ReturnResultFor1Strike2Balls) {
+    check1Strikes2Ball("132");
+    check1Strikes2Ball("321");
+    check1Strikes2Ball("213");
 }
 
 int main() {
