a260eb77bb5a3172e01b0d9f42bb8b4a48736ecc
Taehyun <ohyeaha@hanmail.net>
2025-07-02 13:43:25 +0900

    [refactor] 2s 1b - 1

--- 02_tdd_baseball/baseball.cpp
+++ 02_tdd_baseball/baseball.cpp
@@ -20,21 +20,15 @@ public:
             return { true, 3, 0 };
         }
         
-        if (guessNumber[0] == question[0]
-            && guessNumber[1] == question[1]
-            && guessNumber[2] != question[2]) {
-            return { false, 2, 0 };
-        }
-
-        if (guessNumber[0] == question[0]
-            && guessNumber[1] != question[1]
-            && guessNumber[2] == question[2]) {
-            return { false, 2, 0 };
-        }
-
-        if (guessNumber[0] != question[0]
-            && guessNumber[1] == question[1]
-            && guessNumber[2] == question[2]) {
+        if ((guessNumber[0] == question[0]
+                && guessNumber[1] == question[1]
+                && guessNumber[2] != question[2]) ||
+            (guessNumber[0] == question[0]
+                && guessNumber[1] != question[1]
+                && guessNumber[2] == question[2]) ||
+            (guessNumber[0] != question[0]
+                && guessNumber[1] == question[1]
+                && guessNumber[2] == question[2])){
             return { false, 2, 0 };
         }
 
