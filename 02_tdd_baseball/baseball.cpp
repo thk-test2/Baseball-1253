0902712d56626e0ec75ff9f0213159dab7aaa482
Taehyun <ohyeaha@hanmail.net>
2025-07-02 13:11:44 +0900

    [feature] 39p

--- 02_tdd_baseball/baseball.cpp
+++ 02_tdd_baseball/baseball.cpp
@@ -4,15 +4,21 @@ using namespace std;
 
 class Baseball {
 public:
-    void guess(const string& string) {
-        if (string.length() != 3) {
+    void guess(const string& guessNumber) {
+        if (guessNumber.length() != 3) {
             throw length_error("Must be three letters.");
         }
 
-        for (char ch : string) {
+        for (char ch : guessNumber) {
             if (ch < '0' || ch > '9') {
-                throw invalid_argument("Must be number");
+                throw invalid_argument("Must be numbers");
             }
+        }
+
+        if (guessNumber[0] == guessNumber[1]
+            || guessNumber[0] == guessNumber[2]
+            || guessNumber[1] == guessNumber[2]) {
+            throw invalid_argument("Must not have the same number");
         }
     }
 };
